7c639789cc7518ff9f3b7edbe51ef980af15ff28
Yun Ji <this.jiyun@gmail.com>
2023-03-28 10:10:31 +0200

    brute force solution

--- leeCode/twoSum/twoSum.js
+++ leeCode/twoSum/twoSum.js
@@ -1,20 +1,7 @@
 const twoSum = function (nums, target) {
-  const numsMap = nums.map((num, index) => {
-    return {
-      number: num,
-      index: index
-    }
-  })
-
-  //sort map and exclude the bigger ones
-  const filteredMap = numsMap.filter((e) => e.number <= target)
-  console.log({ numsMap, filteredMap })
-
-  for (let i = 0; i < filteredMap.length; i++) {
-    const complement = target - filteredMap[i].number
-    const complementIndex = filteredMap.findIndex(
-      (n) => n.number === complement
-    )
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i]
+    const complementIndex = nums.findIndex((n) => n === complement)
     if (complementIndex !== -1 && complementIndex !== i) {
       return [i, complementIndex]
       break
